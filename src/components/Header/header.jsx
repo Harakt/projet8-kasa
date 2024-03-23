c4a1cedf9f922fb0ed1c96a398a7dc3952c91aa5
clatot <kat.clatot@gmail.com>
2024-03-23 16:25:47 +0100

    Avancement header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 function Header() {
   return (
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/propos">A Propos</Link>
-    </nav>
+    <NavContainer>
+      <Link to="/">
+        <img src="./src/assets/LOGO.svg" alt="" />
+      </Link>
+      <div>
+        <Link to="/">Accueil</Link>
+        <Link to="/propos">A Propos</Link>
+      </div>
+    </NavContainer>
   )
 }
 
