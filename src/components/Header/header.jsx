3a1742b267e8113fff7075b7295d8ebafaf80797
clatot <kat.clatot@gmail.com>
2024-03-26 02:10:15 +0100

    Ajout Soulignement Header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,15 +1,30 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import './header.scss'
 
 function Header() {
+  const { pathname } = useLocation()
+  const [path, setPath] = useState('')
+
+  useEffect(() => {
+    setPath(pathname)
+  }, [pathname])
+
   return (
-    <nav className="navContainer">
+    <nav className="headerContainer">
       <Link to="/">
         <img src="./src/assets/LOGO.svg" alt="" />
       </Link>
-      <div>
-        <Link to="/">Accueil</Link>
-        <Link to="/propos">A Propos</Link>
+      <div className="headerLinkContainer">
+        <Link className={path === '/' ? 'selectedLink' : 'headerLink'} to="/">
+          Accueil
+        </Link>
+        <Link
+          className={path === '/propos' ? 'selectedLink' : 'headerLink'}
+          to="/propos"
+        >
+          A Propos
+        </Link>
       </div>
     </nav>
   )
