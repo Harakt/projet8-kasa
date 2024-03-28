9eb5d58f889de7c2fe92243846138d6a367cde2c
clatot <kat.clatot@gmail.com>
2024-03-28 02:17:14 +0100

    Ajout Page Error

--- src/components/Error/error.jsx
+++ src/components/Error/error.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Error() {
   return (
     <div>
-      <h1> Error Kasa</h1>
+      <h2>404</h2>
+      <span>Oups! La page que vous demandez n'existe pas.</span>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   )
 }
