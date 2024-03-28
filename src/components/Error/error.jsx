32e57072e7afac2736eccef0c86d8b96cdce0cfa
clatot <kat.clatot@gmail.com>
2024-03-28 15:58:20 +0100

    Style Page Error

--- src/components/Error/error.jsx
+++ src/components/Error/error.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
+import './error.scss'
 
 function Error() {
   return (
-    <div>
-      <h2>404</h2>
-      <span>Oups! La page que vous demandez n'existe pas.</span>
-      <Link to="/">Retourner sur la page d'accueil</Link>
+    <div className="errorContainer">
+      <h2 className="errorCode">404</h2>
+      <span className="errorMessage">
+        Oups! La page que vous demandez n'existe pas.
+      </span>
+      <Link className="errorReturn" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   )
 }
