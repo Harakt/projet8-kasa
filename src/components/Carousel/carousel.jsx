54d1d6dc9ab9aa5488ce8a3b81442704b255f2fc
clatot <kat.clatot@gmail.com>
2024-04-01 16:49:33 +0200

    Ajout Indicator Carousel

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -16,27 +16,41 @@ function Carousel({ pictures }) {
 
   return (
     <div className="carouselContainer">
-      <button
-        className="carouselButton carouselButtonLeft"
-        onClick={handlePrevClick}
-      >
-        <img src="/src/assets/chevron-left.png" alt="" />
-      </button>
-      <div className="carouselPictures">
-        {pictures.map((picture, index) => (
-          <CarouselPicture
-            key={index}
-            picture={picture}
-            active={index === currentIndex}
-          />
-        ))}
-      </div>
-      <button
-        className="carouselButton carouselButtonRight"
-        onClick={handleNextClick}
-      >
-        <img src="/src/assets/chevron-left.png" alt="" />
-      </button>
+      {pictures.length > 1 && (
+        <>
+          <button
+            className="carouselButton carouselButtonLeft"
+            onClick={handlePrevClick}
+          >
+            <img src="/src/assets/chevron-left.png" alt="" />
+          </button>
+          <div className="carouselPictures">
+            {pictures.map((picture, index) => (
+              <CarouselPicture
+                key={index}
+                picture={picture}
+                active={index === currentIndex}
+              />
+            ))}
+          </div>
+          <button
+            className="carouselButton carouselButtonRight"
+            onClick={handleNextClick}
+          >
+            <img src="/src/assets/chevron-left.png" alt="" />
+          </button>
+          <div className="carouselIndicator">
+            <span>
+              {currentIndex + 1} / {pictures.length}
+            </span>
+          </div>
+        </>
+      )}
+      {pictures.length === 1 && (
+        <div className="carouselPictures">
+          <CarouselPicture picture={pictures[0]} active={true} />
+        </div>
+      )}
     </div>
   )
 }
